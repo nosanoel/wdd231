ca61d2ccee6f26bf7b89bb04bbd1baf6135c29d7
unknown <idehennosa231@gmail.com>
2025-12-03 15:43:17 +0100

    jo

--- finalproject/script/esmo.js
+++ finalproject/script/esmo.js
@@ -1,3 +1,3 @@
- import { loadGamesInto } from './script/game.js';
+ import { loadGamesInto } from 'script/game.js';
     // Render a small set into the demo container to show module usage
     loadGamesInto('moduleDemo');
